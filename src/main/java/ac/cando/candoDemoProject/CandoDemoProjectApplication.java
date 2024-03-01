4c59c1c99be7c945aa1c272aecdf7dfcb1d4829b
عرفان طاهری <e.taheri@Candocollege.local>
2024-03-01 11:40:30 +0330

    intial release

---
+++ src/main/java/ac/cando/candoDemoProject/CandoDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package ac.cando.candoDemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandoDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandoDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/cando/candoDemoProject/CandoDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.cando.candoDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandoDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
